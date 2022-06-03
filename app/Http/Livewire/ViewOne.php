0eae65e64733c2ac3153da5db465fada70591171
Spencer Lyons <sjl8r8@gmail.com>
2022-06-03 18:19:18 -0500

    able to repeat the bug now

--- app/Http/Livewire/ViewOne.php
+++ app/Http/Livewire/ViewOne.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class ViewOne extends Component
 {
+    public $input;
+
+    public function handleClick()
+    {
+        error_log($this->input);
+        $this->input = "";
+    }
     public function render()
     {
         return view('livewire.view-one');
--- resources/views/livewire/view-one.blade.php
+++ resources/views/livewire/view-one.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
     <h1>I am page 1</h1>
-    <input type="text" class='flex-1 focus:ring-blue-500 focus:border-blue-500 block w-full min-w-0 sm:text-sm border-gray-300 rounded-md shadow'/>
-    <button class="bg-blue-500 text-white p-2 rounded my-2" onclick="console.log('button clikced')">Click Me</button>
+    <form wire:submit.prevent="handleClick" class="flex flex-col">
+
+    <input type="text" wire:model="input" class='flex-1 focus:ring-blue-500 focus:border-blue-500 block my-4 p-2 w-full min-w-0 sm:text-sm border-gray-300 rounded-md shadow'/>
+    {{-- <button class="bg-blue-500 text-white p-2 rounded my-2" wire:click="handleClick" onclick="console.log('button clikced')">Click Me</button> --}}
+    <button class="bg-blue-500 text-white p-2 rounded my-2" type="submit">Click Me</button>
+    </form>
 </x-layout>
